b05d985bf53b1a068d5c67e7961ff934ff7d2753
Tolga Aktaş <akts.tlga@gmail.com>
2025-10-04 18:44:59 +0300

    Tolga_01 Classı Üçüncü Sout
    Tolga_01 Classı Üçüncü Sout

--- src/test/java/Tolga_01.java
+++ src/test/java/Tolga_01.java
@@ -2,5 +2,6 @@ public class Tolga_01 {
     public static void main(String[] args) {
         System.out.println("İlk Sout");
         System.out.println("İkinci sout");
+        System.out.println("Üçüncü sout");
     }
 }
