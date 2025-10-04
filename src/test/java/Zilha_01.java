a5426e68efaa4e5b7b0a8907a176201b54522265
ZilhaKalac <zilhakalac@gmail.com>
2025-10-04 18:49:49 +0300

    update
    deneme update

---
+++ src/test/java/Zilha_01.java
@@ -0,0 +1,10 @@
+public class Zilha_01 {
+    public static void main(String[] args) {
+
+        System.out.println("Zilha : ilk sout");
+        System.out.println("zilha: ikinci sout");
+        System.out.println("deneme 3");
+        System.out.println("deneme 4");
+        
+    }
+}
