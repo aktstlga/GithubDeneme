8c456c195b92e4c04dbb711b48baaeb0adf5ea11
ZilhaKalac <zilhakalac@gmail.com>
2025-10-04 18:42:26 +0300

    Zilha_01 değişşiklik 2
    yeni değişiklik

--- src/test/java/Zilha_01.java
+++ src/test/java/Zilha_01.java
@@ -2,6 +2,7 @@ public class Zilha_01 {
     public static void main(String[] args) {
 
         System.out.println("Zilha : ilk sout");
+        System.out.println("zilha: ikinci sout");
         
     }
 }
